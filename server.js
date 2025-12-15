96270539c82aadbf282e16fa83d9ec9095d3a83e
Mena <dayjacs03@gmail.com>
2025-12-15 15:54:43 -0600

    initializing base files for proj

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(3000, (error) => {
+  if (!error) {
+    console.log("server is active");
+  }
+  console.log("server not working :(");
+});
