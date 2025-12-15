f792cb1bdf26fdb922e4a592493f8229bda94eee
Mena <dayjacs03@gmail.com>
2025-12-15 15:59:32 -0600

    adding msgs

---
+++ routes/home.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+const msgRouter = Router();
+const msgs = [
+  {
+    msg: "coffee later?",
+    user: "Loreal",
+    added: new Date(),
+  },
+  {
+    msg: "wya??",
+    user: "Milani",
+    added: new Date(),
+  },
+  {
+    msg: "wya??",
+    user: "Bretman R",
+    added: new Date(),
+  },
+  {
+    msg: "wya??",
+    user: "Armani",
+    added: new Date(),
+  },
+  {
+    msg: "wya??",
+    user: "Patrick Ta",
+    added: new Date(),
+  },
+];
