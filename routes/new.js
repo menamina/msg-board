48e69dd86054b4c32686ec6342d70b3c864cf3ef
Mena <dayjacs03@gmail.com>
2025-12-15 17:18:43 -0600

    posting data back to 'database'

--- routes/new.js
+++ routes/new.js
@@ -1,5 +1,15 @@
 const { Router } = require("express");
 const newMsgRouter = Router();
+
 newMsgRouter.get("/new", (req, res) => {
   res.render("form");
+});
+
+newMsgRouter.post("/new", (req, res) => {
+  messages.push({
+    msg: req.body.msg,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.end();
 });
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("node:path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, (error) => {
   if (!error) {
     console.log("server is active");
